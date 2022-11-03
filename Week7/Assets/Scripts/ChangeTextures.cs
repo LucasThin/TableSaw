c7a958cd56856935b52225ccab61d418a4df1925
LucasThin <107634089+LucasThin@users.noreply.github.com>
2022-11-03 13:44:46 -0700

    Added changing vest color functionality in warehouseXR

---
+++ Week7/Assets/Scripts/ChangeTextures.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTextures : MonoBehaviour
+{
+    public Material red, blue, green, gray, orange;
+    private Renderer m_Renderer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Renderer = GetComponent<Renderer>();
+        m_Renderer.enabled = true;
+        m_Renderer.sharedMaterial = gray;
+    }
+
+    // Update is called once per frame
+    public void ChangeToBlue()
+    {
+        Debug.Log("change to blue");
+        m_Renderer.sharedMaterial = green;
+    }
+    
+    public void ChangeToRed()
+    {
+        Debug.Log("change to blue");
+        m_Renderer.sharedMaterial = red;
+    }
+    
+    public void ChangeToGray()
+    {
+        Debug.Log("change to blue");
+        m_Renderer.sharedMaterial = gray;
+    }
+    
+    public void ChangeToGreen()
+    {
+        Debug.Log("change to blue");
+        m_Renderer.sharedMaterial = green;
+    }
+    
+    public void ChangeToOrange()
+    {
+        Debug.Log("change to blue");
+        m_Renderer.sharedMaterial = orange;
+    }
+}
