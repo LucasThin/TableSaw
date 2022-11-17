b932d5c07d78403987af2d107d4e1ebedb5096af
mwang17-prototyping <mwang17@inside.artcenter.edu>
2022-11-16 20:47:31 -0800

    Assign states2

--- Week7/Assets/AssignState.cs
+++ Week7/Assets/AssignState.cs
@@ -9,6 +9,10 @@ public class AssignState : MonoBehaviour
     public GameObject movingTrigger;
     public GameObject liftingTrigger;
     public GameObject carryingTrigger;
+    public GameObject idleText;
+    public GameObject movingText;
+    public GameObject liftingText;
+    public GameObject carryingText;
 
 
     // Start is called before the first frame update
@@ -18,11 +22,31 @@ public class AssignState : MonoBehaviour
         movingTrigger.SetActive(false);
         liftingTrigger.SetActive(false);
         carryingTrigger.SetActive(false);
+
+        idleText.SetActive(false);
+        movingText.SetActive(false);
+        liftingText.SetActive(false);
+        carryingText.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        //if (detectingState.state == movingState.rest)
+        //{
+        //    idleTrigger.SetActive(false);
+        //    movingTrigger.SetActive(false);
+        //    liftingTrigger.SetActive(false);
+        //    carryingTrigger.SetActive(false);
+
+        //    idleText.SetActive(false);
+        //    movingText.SetActive(false);
+        //    liftingText.SetActive(false);
+        //    carryingText.SetActive(false);
+
+        //    Debug.Log("break trigger is on");
+        //}
 
         if (detectingState.state == movingState.idle)
         {
@@ -30,6 +54,11 @@ public class AssignState : MonoBehaviour
             movingTrigger.SetActive(false);
             liftingTrigger.SetActive(false);
             carryingTrigger.SetActive(false);
+
+            idleText.SetActive(true);
+            movingText.SetActive(false);
+            liftingText.SetActive(false);
+            carryingText.SetActive(false);
 
             //Debug.Log("idle trigger is on");
         }
@@ -41,6 +70,11 @@ public class AssignState : MonoBehaviour
             liftingTrigger.SetActive(false);
             carryingTrigger.SetActive(false);
 
+            idleText.SetActive(false);
+            movingText.SetActive(true);
+            liftingText.SetActive(false);
+            carryingText.SetActive(false);
+
             //Debug.Log("moving state is on");
         }
 
@@ -51,6 +85,10 @@ public class AssignState : MonoBehaviour
             liftingTrigger.SetActive(true);
             carryingTrigger.SetActive(false);
 
+            idleText.SetActive(false);
+            movingText.SetActive(false);
+            liftingText.SetActive(true);
+            carryingText.SetActive(false);
 
             //Debug.Log("lifting state is on");
         }
@@ -61,6 +99,11 @@ public class AssignState : MonoBehaviour
             movingTrigger.SetActive(false);
             liftingTrigger.SetActive(false);
             carryingTrigger.SetActive(true);
+
+            idleText.SetActive(false);
+            movingText.SetActive(false);
+            liftingText.SetActive(false);
+            carryingText.SetActive(true);
 
             //Debug.Log("carrying state");
         }
