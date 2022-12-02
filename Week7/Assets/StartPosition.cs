3020ec87c40fcff5502392185998fb19141a6c86
Isabel913 <77878779+Isabel913@users.noreply.github.com>
2022-12-01 18:14:10 -0800

    .

--- Week7/Assets/StartPosition.cs
+++ Week7/Assets/StartPosition.cs
@@ -7,7 +7,7 @@ public class StartPosition : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position =new Vector3 (0.95f, 0.1f, 5.12f);
+        transform.position =new Vector3 (0.95f, 0.11f, 5.12f);
     }
 
     // Update is called once per frame
