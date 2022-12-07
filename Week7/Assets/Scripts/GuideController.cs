9716428f5d86492b2956dad5c70eb57fa7dda8c1
LucasThin <107634089+LucasThin@users.noreply.github.com>
2022-12-07 13:16:48 -0800

    person wait then continue

--- Week7/Assets/Scripts/GuideController.cs
+++ Week7/Assets/Scripts/GuideController.cs
@@ -1,15 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Oculus.Platform;
 using TMPro.EditorUtilities;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class GuideController : MonoBehaviour
 {
+    enum AvatarState
+    {
+        Guiding = 0,
+        Waiting = 1,
+        PickUp = 2,
+        PutDown =3 
+        
+    }
+    
+    
     [SerializeField] AvatarMovement avatarMovement;
     [SerializeField] NavMeshAgent agent;
     [SerializeField] private float _threshold = 0.5f;
     [SerializeField] private GuidePath _guidePath;
+    [SerializeField] private AvatarState _state = AvatarState.Guiding;
+    [SerializeField] private CheckforPlayer _checkforPlayer;
     
     private float _time = 0;
     private bool _moving = false;
@@ -38,23 +51,46 @@ public class GuideController : MonoBehaviour
                 _time += Time.deltaTime;
             }
         } */
+
+
+        if (!_checkforPlayer._playerFollowing)
+        {
+            _state = AvatarState.Waiting;
+            
+        } else if (_checkforPlayer._playerFollowing)
+        {
+            _state = AvatarState.Guiding;
+        }
+
+            
+        if (_state == AvatarState.Guiding)
+        {
+            Guiding();
+            
+        } else if (_state == AvatarState.Waiting)
+        {
+            IsWaiting();
+        }
+        
+    }
+
+    private void IsWaiting()
+    {
+        Debug.Log("Waiting for player to catch up");
+        agent.SetDestination(transform.position);
+        _moving = false;
+
+    }
+
+    // Check if guide is moving or not and setting path point for it
+    private void Guiding()
+    {
+        //if guide isn't moving and has not reached the end, it means it reached the path point. 
         if (!_moving && !_reachedEnd)
         {
-            //if guide isn't moving, it means it reached the path point. 
-            //when it reached the path point, change path point to the next point. 
-            _pathIndex++;
-            
-            //don't go out of the list. Catch argumentException error
-            if (_pathIndex == _guidePath.pathPoints.Count)
-            {
-                //Set the path as the final point 
-                _pathIndex = _guidePath.pathPoints.Count - 1;
-                _reachedEnd = true;
-            }
-            
-            _currentPoint =  _guidePath.pathPoints[_pathIndex];
+            SetNextDestination();
             agent.SetDestination(_currentPoint.position);
-            
+
             //agent have a destination to go hence he will be moving
             _moving = true;
         }
@@ -63,6 +99,21 @@ public class GuideController : MonoBehaviour
         {
             _moving = false;
         }
+    }
 
+    private void SetNextDestination()
+    {
+        //when it reached the path point, change path point to the next point. 
+        _pathIndex++;
+            
+        //don't go out of the list. Catch argumentException error
+        if (_pathIndex == _guidePath.pathPoints.Count)
+        {
+            //Set the path as the final point 
+            _pathIndex = _guidePath.pathPoints.Count - 1;
+            _reachedEnd = true;
+        }
+            
+        _currentPoint =  _guidePath.pathPoints[_pathIndex];
     }
 }
