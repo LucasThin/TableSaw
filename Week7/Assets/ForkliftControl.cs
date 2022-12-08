0f6d051dfdeb07243cca5df9b970eec9b14f7c35
mwang17-prototyping <mwang17@inside.artcenter.edu>
2022-12-08 14:17:45 -0800

    Revert "Merge branch 'main' of https://github.com/LucasThin/Warehouse"
    This reverts commit 601830763f568b15bba4bccf9af60bca54891861, reversing
    changes made to 2c7327b84c640422468f8403200c29f6d392d8a2.

--- Week7/Assets/ForkliftControl.cs
+++ Week7/Assets/ForkliftControl.cs
@@ -5,11 +5,8 @@ using UnityEngine;
 public class ForkliftControl : MonoBehaviour
 {
     [SerializeField] GameObject _forklift;
-    [SerializeField] GameObject _worker;
     private bool triggerForklift = false;
-    private bool triggerWorker = false;
     private bool stopForklift = false;
-    private bool stopWorker = false;
     private float _time = 0;
 
     // Start is called before the first frame update
@@ -21,20 +18,10 @@ public class ForkliftControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (triggerForklift == true)
+        if (triggerForklift == true && stopForklift == false)
         {
-            float _distance = Vector3.Distance(_worker.transform.position, new Vector3 (-3.48f,0f,0.23f));
-            if (_distance > 1f) 
-            {
-                _worker.transform.Translate(Vector3.forward * 1.2f * Time.deltaTime); 
-            }
-
-            if(stopForklift == false)
-            {
-                _forklift.transform.Translate(Vector3.forward * 0.7f * Time.deltaTime);
-            }
+          _forklift.transform.Translate(Vector3.forward * 0.7f * Time.deltaTime);
         }
-
 
     }
 
@@ -42,7 +29,6 @@ public class ForkliftControl : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            triggerForklift = true;
             triggerForklift = true;
         }
         if (other.CompareTag("Forklift"))
