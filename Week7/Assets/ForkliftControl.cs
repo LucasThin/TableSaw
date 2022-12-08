a53d0a224f6abf907a09b7c8c81419edd82a700e
mwang17-prototyping <mwang17@inside.artcenter.edu>
2022-12-08 14:00:23 -0800

    TriggerZone

--- Week7/Assets/ForkliftControl.cs
+++ Week7/Assets/ForkliftControl.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class ForkliftControl : MonoBehaviour
 {
     [SerializeField] GameObject _forklift;
+    [SerializeField] GameObject _worker;
     private bool triggerForklift = false;
+    private bool triggerWorker = false;
     private bool stopForklift = false;
+    private bool stopWorker = false;
     private float _time = 0;
 
     // Start is called before the first frame update
@@ -18,10 +21,20 @@ public class ForkliftControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (triggerForklift == true && stopForklift == false)
+        if (triggerForklift == true)
         {
-          _forklift.transform.Translate(Vector3.forward * 0.7f * Time.deltaTime);
+            float _distance = Vector3.Distance(_worker.transform.position, new Vector3 (-3.48f,0f,0.23f));
+            if (_distance > 1f) 
+            {
+                _worker.transform.Translate(Vector3.forward * 1.2f * Time.deltaTime); 
+            }
+
+            if(stopForklift == false)
+            {
+                _forklift.transform.Translate(Vector3.forward * 0.7f * Time.deltaTime);
+            }
         }
+
 
     }
 
@@ -29,6 +42,7 @@ public class ForkliftControl : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            triggerForklift = true;
             triggerForklift = true;
         }
         if (other.CompareTag("Forklift"))
